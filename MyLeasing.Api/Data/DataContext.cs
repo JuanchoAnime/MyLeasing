7439f9b1a7eaa6d371122276afd6a0914f32893c
Masataka Anime <Judas3991@gmail.com>
2020-10-24 09:20:39 -0500

    add data for first tables in database

---
+++ MyLeasing.Api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLeasing.Api.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyLeasing.Api.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OwnerDto> Owners { get; set; }
+    }
+}
--- MyLeasing.Api/Startup.cs
+++ MyLeasing.Api/Startup.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace MyLeasing.Api
+﻿namespace MyLeasing.Api
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MyLeasing.Api.Data;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,21 +20,17 @@ namespace MyLeasing.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(cfg => {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConection"));
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
+            if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
+            else { app.UseHsts(); }
             app.UseHttpsRedirection();
             app.UseMvc();
         }
