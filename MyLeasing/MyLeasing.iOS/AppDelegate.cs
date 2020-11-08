e64ef948a82586a49a7a3515d683dd5e0f9130c4
Masataka Anime <Judas3991@gmail.com>
2020-11-08 15:20:47 -0500

    iOS ok

--- MyLeasing/MyLeasing.iOS/AppDelegate.cs
+++ MyLeasing/MyLeasing.iOS/AppDelegate.cs
@@ -1,29 +1,18 @@
-﻿using Foundation;
-using Prism;
-using Prism.Ioc;
-using UIKit;
-
-
-namespace MyLeasing.iOS
+﻿namespace MyLeasing.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
+    using Foundation;
+    using Prism;
+    using Prism.Ioc;
+    using UIKit;
+
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             LoadApplication(new App(new iOSInitializer()));
-
             return base.FinishedLaunching(app, options);
         }
     }
@@ -32,7 +21,6 @@ namespace MyLeasing.iOS
     {
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
-            // Register any platform specific implementations
         }
     }
 }
